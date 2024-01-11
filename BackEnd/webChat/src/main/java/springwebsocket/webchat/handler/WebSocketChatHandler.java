0d07814b852c954651a009682f46a2e71c5dbb6d
KangJiSseok <rkdwltjr5876@naver.com>
2024-01-11 14:52:39 +0900

    Feat : Socket 통신 추가

---
+++ BackEnd/webChat/src/main/java/springwebsocket/webchat/handler/WebSocketChatHandler.java
@@ -0,0 +1,39 @@
+package springwebsocket.webchat.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import springwebsocket.webchat.dao.ChatDAO;
+import springwebsocket.webchat.dto.ChatRoom;
+import springwebsocket.webchat.service.ChatService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketChatHandler extends TextWebSocketHandler {
+
+    private final ObjectMapper mapper;
+
+    private final ChatService service;
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+
+        ChatDAO chatMessage = mapper.readValue(payload, ChatDAO.class);
+        log.info("session {}", payload);
+
+        ChatRoom room = service.findRoomById(chatMessage.getRoomId());
+        log.info("room {}", room.toString());
+
+        room.handleAction(session, chatMessage, service);
+
+    }
+}
