aa76dfa765977838e29b78824e6c2604a056b278
KangJiSseok <rkdwltjr5876@naver.com>
2024-01-13 22:53:51 +0900

    Feat : User CRUD add

---
+++ BackEnd/webChat/src/main/java/springwebsocket/webchat/repository/JpaUserRepository.java
@@ -0,0 +1,47 @@
+package springwebsocket.webchat.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import springwebsocket.webchat.dao.User;
+import springwebsocket.webchat.dto.UserUpdateDto;
+
+import java.util.Optional;
+
+@Transactional  // Jpa에서 데이터의 변경이 있을 경우 Transactional 을 써야함
+@Repository
+@Slf4j
+public class JpaUserRepository implements UserRepository{
+
+    private EntityManager em;
+
+    public JpaUserRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public User save(User user) {
+        em.persist(user);
+        return user;
+    }
+
+    @Override
+    public void update(Long userId, UserUpdateDto updateParam) {
+        User findUser = em.find(User.class, userId);
+        findUser.setEmail(updateParam.getEmail());
+        findUser.setPassword(updateParam.getPassword());
+        findUser.setSender(updateParam.getName());
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        User user = em.find(User.class, id);
+        return Optional.ofNullable(user);
+    }
+
+    @Override
+    public void delete(Long id) {
+        em.remove(id);
+    }
+}
---
+++ BackEnd/webChat/src/main/java/springwebsocket/webchat/service/UserServiceV1.java
@@ -0,0 +1,36 @@
+package springwebsocket.webchat.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import springwebsocket.webchat.dao.User;
+import springwebsocket.webchat.dto.UserUpdateDto;
+import springwebsocket.webchat.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceV1 implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void update(Long userId, UserUpdateDto updateParam) {
+        userRepository.update(userId, updateParam);
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        userRepository.delete(id);
+    }
+}
