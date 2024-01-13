627584b3c30b397b39113b2efdc034c03226e3dd
KangJiSseok <rkdwltjr5876@naver.com>
2024-01-14 01:44:48 +0900

    Feat : CRUD Controller add

---
+++ BackEnd/webChat/src/main/java/springwebsocket/webchat/controller/UserController.java
@@ -0,0 +1,41 @@
+package springwebsocket.webchat.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springwebsocket.webchat.dao.User;
+import springwebsocket.webchat.dto.UserUpdateDto;
+import springwebsocket.webchat.service.UserServiceV1;
+
+import java.util.Optional;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserServiceV1 userService;
+
+    @PostMapping("/save")
+    public User save(User user) {
+        return userService.save(user);
+    }
+
+    @PostMapping("/update")
+    public void update(Long userId, UserUpdateDto updateParam) {
+        userService.update(userId, updateParam);
+    }
+
+    @PostMapping("/find")
+    public Optional<User> findById(Long id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping("delete")
+    public void delete(Long id) {
+        userService.delete(id);
+    }
+}
