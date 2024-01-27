d657730ea68e9ce99e5737c500c871b7e2bca0f6
KangJiSseok <rkdwltjr5876@naver.com>
2024-01-27 17:44:27 +0900

    feat : webRTC example 구현

---
+++ BackEnd/webChat/src/main/java/springwebsocket/webchat/config/SocketConfigurer.java
@@ -0,0 +1,17 @@
+package springwebsocket.webchat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import springwebsocket.webchat.handler.SocketHandler;
+
+
+@Configuration
+@EnableWebSocket
+public class SocketConfigurer implements WebSocketConfigurer {
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(new SocketHandler(), "/socket").setAllowedOrigins("*");
+	}
+}
