6b53fb40bccd1c6a2266692fe0044951da13af68
KangJiSseok <rkdwltjr5876@naver.com>
2024-01-29 11:42:48 +0900

    feat : acceptFriendRequestById 친구요청수락 기능 구현

--- BackEnd/webChat/src/main/java/springwebsocket/webchat/repository/springdata/SpringDataJpaFriendshipRepository.java
+++ BackEnd/webChat/src/main/java/springwebsocket/webchat/repository/springdata/SpringDataJpaFriendshipRepository.java
@@ -2,7 +2,11 @@ package springwebsocket.webchat.repository.springdata;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import springwebsocket.webchat.entity.Friendship;
+import springwebsocket.webchat.entity.Member;
+
+import java.util.Optional;
 
 public interface SpringDataJpaFriendshipRepository extends JpaRepository<Friendship,Long> {
+    Optional<Friendship> findByUserIdAndFriendId(Member sender, Member receiver);
 
 }
