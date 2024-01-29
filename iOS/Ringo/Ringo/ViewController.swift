583ae9af587011e6efae3eaa6fc123bcde0d0b66
hhyukjin <vmak0314@naver.com>
2024-01-29 22:38:56 +0900

    Chore: 프로젝트 생성
    close #6

---
+++ iOS/Ringo/Ringo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ringo
+//
+//  Created by 강진혁 on 1/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
