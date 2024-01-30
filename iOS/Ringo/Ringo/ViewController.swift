98d387028f49a675ac7b9452f52edfd4783a3eb6
hhyukjin <vmak0314@naver.com>
2024-01-30 19:49:28 +0900

    Chore: 스토리보드 없애기
    #6

--- iOS/Ringo/Ringo/ViewController.swift
+++ iOS/Ringo/Ringo/ViewController.swift
@@ -12,6 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let sampleLabel: UILabel = {
+            let label = UILabel()
+            label.text = "스토리보드 없이 구현한 ViewController"
+            label.font = .preferredFont(forTextStyle: .title3)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+        
+        view.backgroundColor = .white
+        view.addSubview(sampleLabel)
+        sampleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        sampleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
     }
 
 
