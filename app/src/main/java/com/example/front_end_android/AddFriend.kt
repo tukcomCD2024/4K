401950b6495af80c52344e7c901d99c572c6d7d0
kmseongmin <102861243+kmseongmin@users.noreply.github.com>
2024-01-31 22:24:28 +0900

    design
    UI design

---
+++ app/src/main/java/com/example/front_end_android/AddFriend.kt
@@ -0,0 +1,11 @@
+package com.example.front_end_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddFriend : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_friend)
+    }
+}
