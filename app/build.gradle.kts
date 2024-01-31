6b5c3a8fbe6eb87dea84708c73825cb737da00d1
kmseongmin <102861243+kmseongmin@users.noreply.github.com>
2024-01-31 19:23:18 +0900

    gradle 오류 수정
    오류 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("com.square.retrofit2:retrofit:2.9.0")
-    implementation ("com.square.retrofit2:converter-gson:2.9.0")
-    implementation ("com.square.okhttp3:okhttp:4.8.0")
-    implementation ("com.square.okhttp3:logging-interceptor:4.8.0")
 }
