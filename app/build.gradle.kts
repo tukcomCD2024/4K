14932db767ee471ee0c60dd89a29782abd8f0dd7
kmseongmin <102861243+kmseongmin@users.noreply.github.com>
2024-02-02 17:21:50 +0900

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.front_end_android"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.front_end_android"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
