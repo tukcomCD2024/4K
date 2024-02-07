168ccf8ec6a96666e8cea7edf3b7d40a448c4069
kmseongmin <102861243+kmseongmin@users.noreply.github.com>
2024-02-08 07:17:34 +0900

    Update Calling.kt

--- app/src/main/java/com/example/front_end_android/Calling.kt
+++ app/src/main/java/com/example/front_end_android/Calling.kt
@@ -2,10 +2,20 @@ package com.example.front_end_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.front_end_android.databinding.ActivityCallingBinding
 
 class Calling : AppCompatActivity() {
+
+    private lateinit var binding:ActivityCallingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityCallingBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_calling)
+
+        binding.exitCallBackground.setOnClickListener {
+            finish()
+        }
+
     }
 }
