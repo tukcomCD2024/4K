6e84d9f454d65ed6475e7701a6299a3cf4dc17f8
kmseongmin <102861243+kmseongmin@users.noreply.github.com>
2024-02-09 16:57:57 +0900

    webrtc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.android.volley:volley:1.2.1")
     implementation ("com.google.code.gson:gson:2.10.1")
+    implementation ("org.webrtc:google-webrtc:")
 }
