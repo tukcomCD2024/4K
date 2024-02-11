309c602007dda10237f220fda57001ee737a4cdc
kmseongmin <102861243+kmseongmin@users.noreply.github.com>
2024-02-11 13:25:39 +0900

    통화요청, 통화중
    통화요청, 통화중

--- app/src/main/java/com/example/front_end_android/Calling.kt
+++ app/src/main/java/com/example/front_end_android/Calling.kt
@@ -2,6 +2,7 @@ package com.example.front_end_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.front_end_android.databinding.ActivityCallingBinding
 
 class Calling : AppCompatActivity() {
@@ -12,6 +13,15 @@ class Calling : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCallingBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_calling)
+
+        binding.buttonTest.setOnClickListener {
+            binding.callingPeople1.visibility = View.VISIBLE
+            binding.callingPeople2.visibility = View.VISIBLE
+            binding.nickname1.visibility = View.VISIBLE
+            binding.nickname2.visibility = View.VISIBLE
+            binding.callingPeopleInit.visibility = View.GONE
+            binding.nicknameInit.visibility = View.GONE
+        }
 
         binding.exitCallBackground.setOnClickListener {
             finish()
