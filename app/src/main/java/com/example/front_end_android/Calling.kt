6bc3f79200ebd9c863b7e8e9e18cb5d22a8246fc
kmseongmin <102861243+kmseongmin@users.noreply.github.com>
2024-02-12 14:38:46 +0900

    webrtc

--- app/src/main/java/com/example/front_end_android/Calling.kt
+++ app/src/main/java/com/example/front_end_android/Calling.kt
@@ -6,12 +6,17 @@ import android.view.View
 import com.example.front_end_android.databinding.ActivityCallingBinding
 import com.example.front_end_android.models.MessageModel
 import com.example.front_end_android.util.NewMessageInterface
+import com.example.front_end_android.util.PeerConnectionObserver
+import org.webrtc.IceCandidate
+import org.webrtc.MediaStream
+import org.webrtc.PeerConnection
 
 class Calling : AppCompatActivity(), NewMessageInterface {
 
     private lateinit var binding:ActivityCallingBinding
     private var userName:String?=null
     private var socketRepository:SocketRepository?=null
+    private var rtcClient : RTCClient?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,7 @@ class Calling : AppCompatActivity(), NewMessageInterface {
         setContentView(binding.root)
         init()
 
+        //통화 참가 ui 테스트 코드
         binding.buttonTest.setOnClickListener {
             binding.callingPeople1.visibility = View.VISIBLE
             binding.callingPeople2.visibility = View.VISIBLE
@@ -26,6 +32,13 @@ class Calling : AppCompatActivity(), NewMessageInterface {
             binding.nickname2.visibility = View.VISIBLE
             binding.callingPeopleInit.visibility = View.GONE
             binding.nicknameInit.visibility = View.GONE
+        }
+
+        binding.nicknameInit.setOnClickListener {
+            binding.callingPeopleContainer.visibility = View.GONE
+            binding.linearLayout.visibility = View.GONE
+            binding.exitCallBackground.visibility = View.GONE
+            binding.callLayout.visibility = View.VISIBLE
         }
 
         binding.exitCallBackground.setOnClickListener {
@@ -38,6 +51,18 @@ class Calling : AppCompatActivity(), NewMessageInterface {
         userName = "seongmin"//실제로는 intent로 유저 이름을 받아야함
         socketRepository = SocketRepository(this)
         userName?.let { socketRepository?.initSocket(it) }
+        rtcClient = RTCClient(application, userName!!, socketRepository!!, object : PeerConnectionObserver(){
+            override fun onIceCandidate(p0: IceCandidate?) {
+                super.onIceCandidate(p0)
+            }
+
+            override fun onAddStream(p0: MediaStream?) {
+                super.onAddStream(p0)
+            }
+        })
+
+        rtcClient?.initializeSurfaceView(binding.localView)
+        rtcClient?.startLocalVideo(binding.localView)
     }
 
     override fun onNewMessage(message: MessageModel) {
