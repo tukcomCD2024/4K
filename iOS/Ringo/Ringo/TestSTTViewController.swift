08ede6770712199dfb5c7184d03454e1f9ca6e2a
hhyukjin <vmak0314@naver.com>
2024-02-21 10:55:36 +0900

    Add: test 뷰 생성

---
+++ iOS/Ringo/Ringo/TestSTTViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TestSTTViewController.swift
+//  Ringo
+//
+//  Created by 강진혁 on 2/21/24.
+//
+
+import UIKit
+
+class TestSTTViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
