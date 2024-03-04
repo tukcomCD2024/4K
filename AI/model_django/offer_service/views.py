1008ecdcb9603af787ed0d6df498550c3e4d15ff
edcrfv458 <edcrfv458@naver.com>
2024-03-05 05:47:59 +0900

    RINGO-115 Add: view.py 파일 end point 추가

--- AI/model_django/offer_service/views.py
+++ AI/model_django/offer_service/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from keras.models import load_model
+
+from . preprocess import preprocess_input
+
+# 모델 로드
+model = load_model('offer_service/LSTM_model.h5')
+
+# 방언 문장을 변경하기 위해 POST 응답만 받음
+@api_view(['POST'])
+def convert_dialect_to_standard(request):
+    if request.method == 'POST':
+        dialect_text = request.data.get('dialect_text', '')  
+
+        try:
+            preprocess_text = preprocess_input(dialect_text)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+
+        try:
+            standard_text = model.predict([preprocess_text])[0]
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+        
+        return JsonResponse({'standard_text': standard_text})
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405) 
