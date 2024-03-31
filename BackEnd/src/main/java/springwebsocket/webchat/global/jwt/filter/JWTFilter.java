0571acf40e05e9f2e67b09e89b4d7811a2411c5d
KangJiSseok <rkdwltjr5876@naver.com>
2024-03-31 19:26:02 +0900

    feat: Access Token 검증 로직
    JWTFilter.doFilterInternal 수정

--- BackEnd/src/main/java/springwebsocket/webchat/global/jwt/filter/JWTFilter.java
+++ BackEnd/src/main/java/springwebsocket/webchat/global/jwt/filter/JWTFilter.java
@@ -1,5 +1,6 @@
 package springwebsocket.webchat.global.jwt.filter;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +15,7 @@ import springwebsocket.webchat.global.jwt.dto.CustomUserDetails;
 import springwebsocket.webchat.member.entity.Member;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Optional;
 
 @Slf4j
@@ -30,39 +32,52 @@ public class JWTFilter extends OncePerRequestFilter {
 
         log.info("JWTFilter doFilterInternal");
 
-        //request에서 Authorization 헤더 찾기
-        String authorization = request.getHeader("Authorization");
+        //request에서 access 헤더 찾기
+        String accessToken = request.getHeader("access");
 
-        //Authorization헤더 검증
-        if (authorization == null || !authorization.startsWith("Bearer ")) {
-
-            log.info("token null");
-            filterChain.doFilter(request, response);
-
-            //조건이 해당되면 메소드 종료 (필수)
-            return;
-        }
-
-        log.info("authorization get");
-        //Bearer 부분 제거 후 순수 토큰만 획득
-        String token = authorization.split(" ")[1];
-
-        //토큰 소멸 시간 검증
-        if (jwtUtil.isExpired(token)) {
-            log.info("token expired");
+        //토큰이 없다면 다음 필터로 넘김
+        if (accessToken == null) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        //token에서 username과 role획득
-        String email = jwtUtil.getEmail(token);
-        String role = jwtUtil.getRole(token);
+        //토큰 만료 여부 확인, 만료시 다음 필터로 넘기지 않음
+        try {
+            jwtUtil.isExpired(accessToken);
+        } catch (ExpiredJwtException e) {
+
+            //response body
+            PrintWriter writer = response.getWriter();
+            writer.print("access token expired");
+
+            //response status code
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        //토큰이 access인지 확인 (발급시 페이로드에 명시)
+        String category = jwtUtil.getCategory(accessToken);
+
+        if (!category.equals("access")) {
+
+            //response body
+            PrintWriter writer = response.getWriter();
+            writer.print("invalid access token");
+
+            //response status code
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        // email, role값 획득
+        String email = jwtUtil.getEmail(accessToken);
+        String role = jwtUtil.getRole(accessToken);
+
 
         //memberEntity를 생성하여 값 set
         Member member = new Member();
         member.setEmail(email);
         member.setRole(role);
-        member.setPassword("asdfasfd");
 
         //userDetails에 회원 정보 객체 담기
         CustomUserDetails customUserDetails = new CustomUserDetails(Optional.of(member));
