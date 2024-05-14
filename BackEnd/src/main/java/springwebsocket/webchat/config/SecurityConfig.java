0f3a56a183597f006022ce4cdeac72caf042a600
KANG JISEOK <rkdwltjr5876@naver.com>
2024-05-14 18:49:39 +0900

    fix : 데모테스트 소켓 토큰 인증 제외

--- BackEnd/src/main/java/springwebsocket/webchat/config/SecurityConfig.java
+++ BackEnd/src/main/java/springwebsocket/webchat/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "/member/*").permitAll()
                         .requestMatchers("reissue").permitAll()
+                        .requestMatchers("/signal").permitAll()
                         .requestMatchers("/favicon.ico",
                                 "/error",
                                 "/swagger-ui/**",
