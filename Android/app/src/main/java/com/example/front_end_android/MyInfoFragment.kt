7c721001aaae6663bf8021b7cdd9dff3f9c99bc9
kmseongmin <102861243+kmseongmin@users.noreply.github.com>
2024-05-15 22:16:00 +0900

    myinfo

--- Android/app/src/main/java/com/example/front_end_android/MyInfoFragment.kt
+++ Android/app/src/main/java/com/example/front_end_android/MyInfoFragment.kt
@@ -1,10 +1,19 @@
 package com.example.front_end_android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.front_end_android.databinding.FragmentFriendsBinding
+import com.example.front_end_android.databinding.FragmentMyInfoBinding
+import com.example.front_end_android.util.AuthInterceptor
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +29,7 @@ class MyInfoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var binding: FragmentMyInfoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +43,32 @@ class MyInfoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding= FragmentMyInfoBinding.inflate(inflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_info, container, false)
+
+        val accessToken = MyApplication.preferences.getString("AccessToken",".")
+        val refreshToken = MyApplication.preferences.getString("RefreshToken",".")
+        val userEmail = MyApplication.preferences.getString("email",".")
+        Log.d("YMC", "access token : $accessToken")
+        Log.d("YMC", "Refresh token : $refreshToken")
+        Log.d("YMC", "email : $userEmail")
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor(accessToken))
+            .build()
+
+        val gson = GsonBuilder().setLenient().create()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://4kringo.shop:8080/")
+            .client(client)
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+
+
+        return binding.root
+        //return inflater.inflate(R.layout.fragment_my_info, container, false)
     }
 
     companion object {
