4e0a587f63b90b47b565d75b68608aec3af5704e
edcrfv458 <edcrfv458@naver.com>
2024-05-15 18:19:50 +0900

    del: 장고 파일 제거

--- AI/model_django/offer_service/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OfferServiceConfig(AppConfig):
-    name = 'offer_service'
--- AI/model_django/offer_service/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import convert_dialect_to_standard
-
-urlpatterns = [
-    path('convert', convert_dialect_to_standard),
-]
