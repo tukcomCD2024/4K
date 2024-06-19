234f344d94ab4cd89745d20405e624b46b74e37e
kmseongmin <102861243+kmseongmin@users.noreply.github.com>
2024-06-19 13:47:48 +0900

    firebase

--- Android/app/src/main/java/com/example/front_end_android/TestActivity.kt
+++ Android/app/src/main/java/com/example/front_end_android/TestActivity.kt
@@ -4,6 +4,7 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -14,49 +15,15 @@ class TestActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityTestBinding
     private val PERMISSION_REQUEST_CODE = 5000
-
-    private fun permissionCheck() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            val permissionCheck = ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.POST_NOTIFICATIONS
-            )
-            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
-                    PERMISSION_REQUEST_CODE
-                )
-            }
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTestBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        permissionCheck()
-
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            PERMISSION_REQUEST_CODE -> {
-                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(applicationContext, "Permission is denied", Toast.LENGTH_SHORT)
-                        .show()
-                } else {
-                    Toast.makeText(applicationContext, "Permission is granted", Toast.LENGTH_SHORT)
-                        .show()
-                }
-            }
-        }
+    fun requestPermission(view: View){
+        
     }
 
 }
